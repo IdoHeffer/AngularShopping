82440194f3eb183f04396e146f8556ca04d2ce07
IdoHeffer <61251487+IdoHeffer@users.noreply.github.com>
2020-04-14 22:54:35 +0300

    Validation function isprodtwiceincart Update Worksss

--- Server/dao/cartItems-dao.js
+++ Server/dao/cartItems-dao.js
@@ -17,6 +17,10 @@ async function getAllCartItems(CartID) {
 }
 
 async function addCartItem(cartItem) {
+    let checktwice = await IsProdTwiceINCart(cartItem);
+    if (checktwice==true) {
+        return;
+    }
    var sql = ("INSERT INTO `marketproject`.`cartitems` (`ProductID`, `Amount`, `TotalItemPrice`, `CartID`)  VALUES (?,?,?,?)")
    let parameters = [cartItem.ProductID,cartItem.Amount,cartItem.TotalItemPrice,cartItem.CartID]
    console.log(parameters);
@@ -24,12 +28,13 @@ async function addCartItem(cartItem) {
    console.log("Cart Created in the DB :"+addeCartItem);
 }
 async function updateCartItem(cartItem) {
-    var sql = ("UPDATE marketproject.cartitems SET ProductID = ?,Amount = ?,TotalItemPrice = ? , CartID= ?  WHERE CartItemID = ? ");
-    let parameters = [cartItem.ProductID,cartItem.Amount,cartItem.TotalItemPrice,cartItem.CartID,cartItem.CartItemID]
+    var sql = ("UPDATE marketproject.cartitems SET Amount = ?,TotalItemPrice = ?  WHERE CartID= ? and ProductID= ?");
+    let parameters = [cartItem.Amount,cartItem.TotalItemPrice,cartItem.CartID,cartItem.ProductID,]
     let updatedCart = await connection.executeWithParameters(sql,parameters);
     console.log("Cart Created in the DB :"+updatedCart)
     return updatedCart;
 }
+
 async function deleteCartItem(id) {
     var sql = ("DELETE FROM cartitems WHERE CartItemID =?");
     let parameters = [id] 
@@ -52,6 +57,25 @@ async function getAllCartItems(CartID) {
     return cartItems;
 }
 
+// func that check if user is trying to add existing product on cart. 
+// it should be updated not added.
+async function IsProdTwiceINCart(cartItem){
+    let sql = ("SELECT * FROM cartitems WHERE CartID=?")
+    let parameters = [cartItem.CartID];
+    let currentcartItems = await connection.executeWithParameters(sql,parameters);
+    console.log(currentcartItems);
+    for (let index = 0; index < currentcartItems.length; index++) {
+        if (cartItem.ProductID==currentcartItems[index].ProductID) {
+            updateCartItem(cartItem);
+            console.log("product exists in this cart it us being updated and not added");
+            return true;
+        }
+        
+    }
+
+    return false;
+
+}
 
 let cartItem = {ProductID:"5",Amount:"7",TotalItemPrice:"25",CartID:"5",CartItemID:"1"};
 // getCartItem(cartItem)
