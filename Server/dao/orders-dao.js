8c37e2c3e812fd7d03e86f8e6f6742eb4a36abe2
IdoHeffer <61251487+IdoHeffer@users.noreply.github.com>
2020-05-07 21:28:01 +0300

    Orders work complitly

--- Server/dao/orders-dao.js
+++ Server/dao/orders-dao.js
@@ -101,11 +101,13 @@ async function getNumberAllOrders(){
 async function getClosedOrder(cartID) {
 
     try{
-        var sql = ("SELECT p.ProductName, p.img, ci.Amount, p.Price, ci.CartItemID, ci.TotalItemPrice, c.CartCreationDate, FROM products p  JOIN cartitems ci ON ci.ProductID=p.ProductID JOIN carts c ON ci.CartID=c.CartID WHERE CartID =?") 
+        var sql = ("SELECT p.ProductName, p.img, ci.Amount, p.Price, ci.CartItemID, ci.TotalItemPrice, c.CartCreationDate FROM products p  JOIN cartitems ci ON ci.ProductID=p.ProductID JOIN carts c ON ci.CartID=c.CartID WHERE c.CartID =?") 
         let parameters = [cartID];
         let cartData = await connection.executeWithParameters(sql,parameters);
         console.log(cartData);
-        console.log(id);
+        console.log("This down here is the ID PROVIDED");
+        
+        console.log(cartID);
         return cartData;
     }catch{
         cartData = [];
