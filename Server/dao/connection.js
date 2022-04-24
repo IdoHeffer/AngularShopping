cc27c0a2bb468973f7b96bd589fb9329aed5d7f9
IdoHeffer <61251487+IdoHeffer@users.noreply.github.com>
2022-04-24 22:36:22 +0300

    Minor changes

--- Server/dao/connection.js
+++ Server/dao/connection.js
@@ -48,7 +48,7 @@ function executeWithParameters(sql,parameters) {
             resolve(result);
         });
     });
- }
+}
 
 
 function isCartForUSer(sql,parameters) {
--- Server/middleware/login-filter.js
+++ Server/middleware/login-filter.js
@@ -7,6 +7,8 @@ let { secret } = config;
 
 function authenticateJwtRequestToken() {
     // Load secret into 
+    // IN the future please add algorithms to the params near @secret at expressJwt Function.
+    // {@link https://github.com/auth0/express-jwt/blob/5fb8c88067b9448d746d04ab60ad3b1996c7e310/README.md#required-parameters}
     return expressJwt({ secret }).unless({
         path: [
             // public routes that don't require authentication
